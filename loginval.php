ba091f2028d05b16bab8bf31df0d60b5cc68ac9b
vgpradip05 <vgpradip05@gmail.com>
2017-04-24 12:08:43 +0530

    Add files via upload

---
+++ loginval.php
@@ -0,0 +1,89 @@
+{
+"Exemption u/s 10":[0:"House Rent(Paid to Landlord)"],
+"Exemption 17(2)":[0:"Medical bills"],
+"Exemption 10(5)":[0:"Leave Travel Allowance"],
+"Exemption u/s 24":[0:"Interest on Housing Loan Replayments"],
+"Exemption u/s 80 & 80C":
+[
+0:"Life Insurance Premiums",
+1:"Public Providant Fund",
+2:"Unit Linked Insurance Policy",
+3:"National Savings Certificate",
+4:"Equity Linked Savings Scheme",
+5:"Equity Linked Mutual Fund",
+6:"Replayment of Housing Loan",
+7:"Children Tution Fee",
+8:"Pension Plan",
+9:"Registration Charge",
+10:"Fixed Deposites",
+11:"Sukanya Samriddhi Account",
+12:"Any Other"
+],
+"Exemption u/s 80D":
+[
+0:"Medical Expences for Handicapped Dependent",
+1:"Medical Premium Paid",
+2:"Parents Medicalim Premium Paid",
+3:"Any Other"
+],
+
+"Exemption u/s 80 CCG":[0 :"Rajiv Gandhi Equity Scheme"],
+"Exemption u/s 80 G /80GGA":[0:"Donation of certian funds",1:"Donation for Scientific Research"],
+
+
+}
+
+
+
+{
+	"Exemption u/s 10": ["House Rent(Paid to Landlord)"],
+	"Exemption 17(2)": ["Medical bills"],
+	"Exemption 10(5)": ["Leave Travel Allowance"],
+	"Exemption u/s 24": ["Interest on Housing Loan Replayments"],
+	"Exemption u/s 80 & 80C": [
+		"Life Insurance Premiums",
+		"Public Providant Fund",
+		"Unit Linked Insurance Policy",
+		"National Savings Certificate",
+		"Equity Linked Savings Scheme",
+		"Equity Linked Mutual Fund",
+		"Replayment of Housing Loan",
+		"Children Tution Fee",
+		"Pension Plan",
+		"Registration Charge",
+		"Fixed Deposites",
+		"Sukanya Samriddhi Account",
+		"Any Other"
+	],
+	"Exemption u/s 80D": [
+		"Medical Expences for Handicapped Dependent",
+		"Medical Premium Paid",
+		"Parents Medicalim Premium Paid",
+		"Any Other"
+	],
+
+	"Exemption u/s 80 CCG": ["Rajiv Gandhi Equity Scheme"],
+	"Exemption u/s 80 G /80GGA": ["Donation of certian funds", "Donation for Scientific Research"]
+}
+
+
+
+{
+	"Schema": [{
+		"Exemptionu/s10": ["HouseRent(PaidtoLandlord)"]
+	}, {
+		"Exemption17(2)": ["Medicalbills"]
+	}, {
+		"Exemption10(5)": ["LeaveTravelAllowance"]
+	}, {
+		"Exemptionu/s24": ["InterestonHousingLoanReplayments"]
+	}, {
+		"Exemptionu/s80&80C": ["LifeInsurancePremiums", "PublicProvidantFund", "UnitLinkedInsurancePolicy", "NationalSavingsCertificate", "EquityLinkedSavingsScheme", "EquityLinkedMutualFund", "ReplaymentofHousingLoan", "ChildrenTutionFee", "PensionPlan", "RegistrationCharge", "FixedDeposites", "SukanyaSamriddhiAccount", "AnyOther"]
+	}, {
+		"Exemptionu/s80D": ["MedicalExpencesforHandicappedDependent", "MedicalPremiumPaid", "ParentsMedicalimPremiumPaid", "AnyOther"]
+	}, {
+		"Exemptionu/s80CCG": ["RajivGandhiEquityScheme"]
+	}, {
+		"Exemptionu/s80G/80GGA": ["Donationofcertianfunds", "DonationforScientificResearch"]
+	}]
+}
